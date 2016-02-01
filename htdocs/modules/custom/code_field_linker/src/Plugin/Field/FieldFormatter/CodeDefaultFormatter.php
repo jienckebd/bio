78402cf7e245e6884a6d226ea978205ada691cb9
Bryan Jiencke <bryan.jiencke@milespartnership.com>
2016-02-01 12:28:45 -0500

    Updates

--- htdocs/modules/custom/code_field_linker/src/Plugin/Field/FieldFormatter/CodeDefaultFormatter.php
+++ htdocs/modules/custom/code_field_linker/src/Plugin/Field/FieldFormatter/CodeDefaultFormatter.php
@@ -26,18 +26,14 @@ class CodeDefaultFormatter extends FormatterBase {
   /**
    * {@inheritdoc}
    */
-  public function viewElements(FieldItemListInterface $items) {
+  public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = array();
+    $settings = $this->getFieldSettings();
 
-    // The ProcessedText element already handles cache context & tag bubbling.
-    // @see \Drupal\filter\Element\ProcessedText::preRenderText()
     foreach ($items as $delta => $item) {
-      $elements[$delta] = array(
-        '#type' => 'processed_text',
-        '#text' => $item->value,
-        '#format' => $item->format,
-        '#langcode' => $item->getLangcode(),
-      );
+      $output = '';
+
+      $elements[$delta] = array('#markup' => $output);
     }
 
     return $elements;
